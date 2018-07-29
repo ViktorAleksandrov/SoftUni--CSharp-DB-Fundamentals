c269b616d4c239f4e26c99aeeff0b16bc7c69f29
ViktorAleksandrov <vikinga0007@gmail.com>
2018-07-29 18:17:59 +0300

    Added 02.ORM Fundamentals

---
+++ Databases Advanced - Entity Framework/02. ORM Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+    using System.Linq;
+    using Data;
+    using Data.Entities;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            const string connectionString = @"Server=VIKTOR-PC\SQLEXPRESS;Database=MiniORM;Integrated Security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+            
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            Employee employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
